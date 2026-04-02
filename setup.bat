dfd446b078f3d3e860299cb7d13dbea6b0da7b00
Stephen Kyle <sk2389@cam.ac.uk>
2026-04-02 12:28:05 +0100

    install markitdown[all] in setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 echo 'prepare Windows preinstall'
 echo 'Installing Python dependencies for OCR...'
 python -m venv .venv
-.venv\Scripts\pip install "markitdown>=0.1.5"
+.venv\Scripts\pip install "markitdown[all]>=0.1.5"
 echo 'Finished installing Python dependencies'
